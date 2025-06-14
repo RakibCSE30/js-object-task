00557bcff42ff2c8ff929644329dc9d63311c875
rakibul <rakibul1810130@gmail.com>
2025-06-14 23:05:57 +0600

    first commit

---
+++ task-1.js
@@ -0,0 +1,16 @@
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+const colors={
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+    golden_rod: '#daa520'
+}
+colors.yello='#2012';
+console.log(colors);
+
+console.log(colors.golden_rod);
