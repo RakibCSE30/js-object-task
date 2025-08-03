de58baeb955cfce02105f6e9fbe44da36832c3a5
rakibul <rakibul1810130@gmail.com>
2025-08-03 22:17:55 +0600

    Recape for Java Script

---
+++ afo.js
@@ -0,0 +1,61 @@
+//  Array declaration 
+const number = [23,432,21,321];
+// print array using loop(for of)
+for(num of number)
+{
+    console.log(num);
+    
+}
+
+//................... function.................. 
+// 1. Normal function
+function addTwoNum(num1,num2){
+    return num1+num2;
+
+}
+console.log('The sum of the number is: ');
+
+console.log(addTwoNum(2,6));
+
+// 2.Arrow function
+console.log('Now visite array function');
+// no parameters
+ const addTwo=()=>55;
+console.log(addTwo());
+// One parameters
+const onePara= a =>a*2;
+console.log(onePara(3));
+// multiple parameters
+const addThreeNumber=(a,b,c)=>a+b+c;
+console.log(addThreeNumber(2,3,5));
+
+// for bracket
+const mulpara=(a,b,c)=>{
+    return a*b*c;
+}
+console.log(mulpara(2,6,5));
+
+
+
+// array copy 
+const currentNumber=[23,435,546,34];
+const copyNumber=currentNumber;
+const spradeOperator=[...currentNumber,9201,3];
+currentNumber.push(65);
+console.log('The copy array is');
+console.log(currentNumber);
+console.log(copyNumber);
+// Its create a problem because of the arrray is not premitive data type.
+// if you can change one it can change it's reference 
+// if you update then the reference has been same location
+//   ................... Solution is ...Operator
+
+
+console.log(spradeOperator);
+
+
+
+
+
+
+
